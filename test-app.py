89d2d30953c2d664af7764fa1dabecd5613cda74
KamaljeethAnand <160145162+KamaljeethAnand@users.noreply.github.com>
2025-07-01 23:01:24 +0530

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -6,11 +6,12 @@ class TodoTestCase(unittest.TestCase):
         self.client = app.test_client()
 
     def test_add_and_get_todo(self):
-        response = self.client.post('/todos', json={'task': 'Learn Jenkins'})
-        self.assertEqual(response.status_code, 201)
+    response = self.client.post('/todos', json={'task': 'Learn Jenkins'})
+    self.assertEqual(response.status_code, 201)
 
-        response = self.client.get('/todos')
-        self.assertIn('Learn Jenkins', response.get_data(as_text=True))
+    response = self.client.get('/todos')
+    self.assertEqual(response.status_code, 200) 
+    self.assertIn('Learn Jenkins', response.get_data(as_text=True))
 
     def test_add_todo_without_task(self):
         response = self.client.post('/todos', json={})
