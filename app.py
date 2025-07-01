74c64eabd1b57a691902e4dcb390da4fa7fcb0fa
KamaljeethAnand <1ms24scs009-t@msrit.edu>
2025-07-01 22:27:23 +0530

    all files

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos), 200
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.json
+    if 'task' not in data:
+        return jsonify({'error': 'Task is required'}), 400
+    todos.append({'task': data['task']})
+    return jsonify({'message': 'Todo added'}), 201
+
+@app.route('/todos/<int:index>', methods=['DELETE'])
+def delete_todo(index):
+    try:
+        todos.pop(index)
+        return jsonify({'message': 'Todo deleted'}), 200
+    except IndexError:
+        return jsonify({'error': 'Invalid index'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test-app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class TodoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_add_and_get_todo(self):
+        response = self.client.post('/todos', json={'task': 'Learn Jenkins'})
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get('/todos')
+        self.assertIn('Learn Jenkins', response.get_data(as_text=True))
+
+    def test_add_todo_without_task(self):
+        response = self.client.post('/todos', json={})
+        self.assertEqual(response.status_code, 400)
+
+if __name__ == '__main__':
+    unittest.main()
